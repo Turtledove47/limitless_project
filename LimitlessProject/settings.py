dd8e12eb606fa449ac22807c5c179e5baa567881
DESKTOP-KAT5EI3\Stefan <st.palamariu@gmail.com>
2023-08-07 20:06:28 +0300

    changed settings

--- LimitlessProject/settings.py
+++ LimitlessProject/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-590vh0jxyfvmdkq^@b#6i@s9w*o(y&lmzye1w2@16n)unb2b#w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+	'limitlessfpv.pythonanywhere.com',
+	'127.0.0.1'
+]
 
 # Application definition
 
